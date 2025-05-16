6816b022a77f639f4f3fac9490c46a3d367c12b6
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-16 12:50:47 -0300

    fix: ledcWrite calls

--- lib/DCMotor/DCMotor.cpp
+++ lib/DCMotor/DCMotor.cpp
@@ -14,23 +14,23 @@ DCMotor::DCMotor(uint8_t in1, uint8_t in2): spd(255) {
   ledcAttach(pin1, PWM_FREQUENCY, PWM_RESOLUTION);
   ledcAttach(pin2, PWM_FREQUENCY, PWM_RESOLUTION);
 
-  ledcWrite(pinChannel1, 0);
-  ledcWrite(pinChannel2, 0);
+  ledcWrite(pin1, 0);
+  ledcWrite(pin2, 0);
 }
 
 void DCMotor::forward() {
-  ledcWrite(pinChannel1, spd);
-  ledcWrite(pinChannel2, 0);
+  ledcWrite(pin1, spd);
+  ledcWrite(pin2, 0);
 }
 
 void DCMotor::backward() {
-  ledcWrite(pinChannel1, 0);
-  ledcWrite(pinChannel2, spd);
+  ledcWrite(pin1, 0);
+  ledcWrite(pin2, spd);
 }
 
 void DCMotor::stop() {
-  ledcWrite(pinChannel1, 0);
-  ledcWrite(pinChannel2, 0);
+  ledcWrite(pin1, 0);
+  ledcWrite(pin2, 0);
 }
 
 void DCMotor::speed(uint8_t speed) {

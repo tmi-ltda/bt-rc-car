0781ee36f6005c9050ac63787cf7e52257ff7140
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-17 18:03:17 -0300

    refactor: updated car class

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include <Car.h>
+#include <Car.hpp>
 
 #define IN1 32
 #define IN2 33
 #define IN3 25
 #define IN4 26
 
-// instância do carro
-Car car(IN1, IN2, IN3, IN4);
+// instância e configuração do carro
+static car_config_t config = {
+  .left_pin1 = IN1,
+  .left_pin2 = IN2,
+  .right_pin1 = IN3,
+  .right_pin2 = IN4
+};
+Car car(config);
 
 // instância do bluetooth
 BluetoothSerial bt;
@@ -39,11 +45,11 @@ void loop() {
       case 'R':
         car.right();
         break;
-      case 'E':
-        car.forwardLeft();
-        break;
-      case 'Q':
-        car.forwardRight();
+      // case 'E':
+      //   car.forwardLeft();
+      //   break;
+      // case 'Q':
+      //   car.forwardRight();
         break;
       case 'S':
         car.stop();
